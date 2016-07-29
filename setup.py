968995b53621324aa0113069dc407215ef29e93f
spohara79 <spohara@gmail.com>
2016-07-28 21:46:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,42 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+import codecs
+import os
+import re
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    return codecs.open(os.path.join(here, *parts), 'r').read()
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
-        return version_match.group()
+        return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = read('README.rst')
 
 setup(
     name='estreamer',
-    version=find_version('estreamer', '__init__.py',
+    version=find_version('estreamer', '__init__.py'),
     description='SourceFire eStream client Python library',
     long_description=long_description,
     url='https://github.com/spohara79/estreamer/',
     author="Sean O'Hara",
     author_email='spohara@gmail.com',
-    license='Apache Software License 2.0',
+    license='Apache Software License',
     zip_safe=False,
-    packages=['estreamer']
+    packages=['estreamer'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
     ],
 
-    keywords='sample setuptools development',
-    install_requires=['pyOpenSSL>=0.15.1'],
+    keywords='sourcefire estreamer',
+    install_requires=['pyOpenSSL>=0.15.1', 'six>=1.10.0'],
 )
